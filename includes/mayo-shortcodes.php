29337a42d9d17be11e128d1bd6cd9136a8af37b9
Randall Rodrigues <randman.rjr@gmail.com>
2020-10-26 01:07:46 -0400

    Added mayo button shortcode to package.

--- includes/mayo-shortcodes.php
+++ includes/mayo-shortcodes.php
@@ -18,3 +18,58 @@ if (! function_exists('star_rating')) {
 if (function_exists('star_rating')) {
     add_shortcode('starrating', 'star_rating');
 }
+
+if ( ! function_exists('mayo_button_shortcode')) {
+    function mayo_button_shortcode($atts, $content = null) {
+        // Extract shortcode attributes
+        extract( shortcode_atts( array(
+            'url'    => '',
+            'class'  => '',
+            'title'  => '',
+            'target' => '',
+            'text'   => '',
+        ), $atts ) );
+
+        // Use text value for items without content
+        $content = $text ? $text : $content;
+
+        // Return button with link
+        if ( $url ) {
+
+            $link_attr = array(
+                'href'   => esc_url( $url ),
+                'class'  => ('' == $class) ? 'btn btn-primary' : $class,
+                'title'  => esc_attr( $title ),
+                'target' => ( 'blank' == $target ) ? '_blank' : '',
+            );
+
+            $link_attrs_str = '';
+
+            foreach ( $link_attr as $key => $val ) {
+
+                if ( $val ) {
+
+                    $link_attrs_str .= ' ' . $key . '="' . $val . '"';
+
+                }
+
+            }
+
+
+            return '<a' . $link_attrs_str . '><span>' . do_shortcode( $content ) . '</span></a>';
+
+        }
+
+        // No link defined so return button as a span
+        else {
+
+            $span_class = ('' == $class) ? 'btn btn-primary' : $class;
+            return '<span class="'.$span_class.'"><span>' . do_shortcode( $content ) . '</span></span>';
+
+        }
+    }
+}
+
+if (function_exists('mayo_button_shortcode')) {
+    add_shortcode('mayo_button','mayo_button_shortcode');
+}
